106361aa5edd5b1052c6d1fa147eb734374c1019
Seweryn Gładysz <seweryn.gladysz@kotrak.com>
2023-01-14 22:58:56 +0100

    add string and tests

---
+++ personal.finance.domain/String.fs
@@ -0,0 +1,11 @@
+﻿namespace personal.finance.domain
+
+module String =
+    let someString = "String use double quotes"
+
+    let stringIncludeEscape = @"\'"
+    let stringNotIncludeEscape = "\'"
+
+    let concateString = "Hello" + " " + "functional world"
+
+
---
+++ personal.finance.tests/String.fs
@@ -0,0 +1,32 @@
+﻿namespace personal.finance.test
+
+open NUnit.Framework
+open personal.finance.domain.String
+
+[<TestFixture>]
+type StringTest () =
+
+    [<Test>]
+    member this.ShouldPass() =
+        Assert.Pass()
+
+    [<Test>]
+    member this.Should_Contains_Escape_Characters() =
+        // act
+        let containSlash = stringIncludeEscape.Contains @"\"
+        let containQuote = stringIncludeEscape.Contains @"'"
+
+        // assert
+        Assert.True(containSlash);
+        Assert.True(containQuote);
+
+    [<Test>]
+    member this.Should_Not_Contains_Escape_Characters() =
+        // act
+        let containSlash = stringNotIncludeEscape.Contains @"\"
+        let containQuote = stringNotIncludeEscape.Contains @"'"
+
+        // assert
+        Assert.False(containSlash);
+        Assert.True(containQuote);
+        
