0f3cc27acb3157e48abd1d6b2868e5b4b0702fcd
Seweryn Gładysz <seweryn.gladysz@kotrak.com>
2023-01-14 22:58:30 +0100

    add immutable and tests

---
+++ personal.finance.domain/Immutable.fs
@@ -0,0 +1,20 @@
+﻿namespace personal.finance.domain
+
+module Immutable =
+
+    let BasicFunction (x: int) =
+        // default bindings are immutable
+        let immutableNumber: int = x
+
+        // if bindings should be muttable add proper keyword
+        // functional language doesn't like that
+        let mutable mutableNumber: int = x
+
+        // this instruction doesn't return any value, without
+        // next instruction this function would return nil/null
+        mutableNumber <- immutableNumber + mutableNumber
+        
+        // notice that, to return value it should be at the end of function
+        mutableNumber
+
+
---
+++ personal.finance.tests/Immutable.fs
@@ -0,0 +1,26 @@
+﻿namespace personal.finance.tests
+
+open NUnit.Framework
+open personal.finance.domain.Immutable
+
+[<TestFixture>]
+type ImmutableTest() =
+
+    [<Test>]    
+    member this.Should_Pass() =
+        Assert.Pass();
+
+    [<Test>]
+    [<TestCase(1)>]
+    [<TestCase(12)>]
+    [<TestCase(-12)>]
+    member this.Should_Return_Valid_Result(value: int) =
+        // act
+        let validResult = value * 2
+
+        // assert
+        let result = BasicFunction value
+
+        // assert
+        Assert.AreEqual(validResult, result)
+        
