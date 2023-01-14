ec2ead92e0ff406453068eb059408eecca660157
Seweryn Gładysz <seweryn.gladysz@kotrak.com>
2023-01-14 22:58:39 +0100

    add booleans and tests

---
+++ personal.finance.domain/Booleans.fs
@@ -0,0 +1,10 @@
+﻿namespace personal.finance.domain
+
+module Booleans =
+    
+    let trueValue = true
+    let falseValue = false
+
+    // as like C# in F# we have && <-> and, || <-> or
+    let result = trueValue && falseValue || trueValue;
+    
---
+++ personal.finance.tests/Booleans.fs
@@ -0,0 +1,15 @@
+﻿namespace personal.finance.tests
+
+open NUnit.Framework
+open personal.finance.domain.Booleans
+
+[<TestFixture>]
+type BooleansTest () =
+    
+    [<Test>]
+    // member need have parenthess in opposition to let functions
+    member this.Should_Pass () =
+        Assert.Pass()
+
+    member this.Should_Be_True () =
+        Assert.True(result)
