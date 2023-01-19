718d3e240f718f9f7bdb09e315a86b7541a3c9a3
Seweryn Gładysz <seweryn.gladysz@kotrak.com>
2023-01-19 21:57:25 +0100

    add struct tuples and tests

---
+++ personal.finance.domain/StructTuples.fs
@@ -0,0 +1,10 @@
+﻿namespace personal.finance.domain
+
+module StructTuples =
+    
+    // struct tuple behave like struct from C#
+    let structTuple = struct (1, 3);
+
+    let convertFromStructTuple struct(a, b) = (a, b);
+
+    let convertToStructTuple (a, b) = struct(a, b);
---
+++ personal.finance.tests/StructTuples.fs
@@ -0,0 +1,56 @@
+﻿namespace personal.finances.tests
+
+open NUnit.Framework
+open personal.finance.domain.StructTuples
+
+[<TestFixture>]
+type StructTuples() =
+    
+    [<Test>]
+    member this.Shoud_Pass() =
+        Assert.Pass();
+
+    [<Test>]
+    [<TestCase(3, 2)>]
+    [<TestCase(-1, -1)>]
+    [<TestCase(1, -1)>]
+    member this.Should_Convert_From_Struct_Tuple(a: int, b: int) =
+        // arrange
+        let structTuple = struct (a, b);
+        let sut = convertFromStructTuple;
+
+        // act
+        let tupleA = sut(structTuple);
+        let tupleB = sut(structTuple);
+
+        let referenceEqualityResult = obj.ReferenceEquals(tupleA, tupleB);
+        let referenceTupleAEqualityResult = obj.ReferenceEquals(tupleA, tupleA);
+        let valueEqualityResult = tupleA = tupleB;
+
+        // assert
+        Assert.False(referenceEqualityResult);
+        Assert.True(referenceTupleAEqualityResult);
+        Assert.True(valueEqualityResult);
+
+    [<Test>]
+    [<TestCase(3, 2)>]
+    [<TestCase(-1, -1)>]
+    [<TestCase(1, -1)>]
+    member this.Should_Convert_To_Struct_Tuple(a: int, b:int) =
+        // arrange
+        let tuple = (a, b);
+        let sut = convertToStructTuple;
+
+        // act
+        let tupleA = sut(tuple);
+        let tupleB = sut(tuple);
+
+        // obj.ReferenceEquals always return false for value types
+        let referenceTupleAEqualityResult = obj.ReferenceEquals(tupleA, tupleA);
+        let valueEqualityResult = tupleA = tupleB;
+
+        // assert
+        Assert.False(referenceTupleAEqualityResult);
+        Assert.True(valueEqualityResult);
+        
+
